7a7d2b5fcc91e0cd0ebee86ff8dbe3cc8a3daeb2
azasyu <azaliasyuanidha.2020@students.amikom.ac.id>
2021-10-13 22:26:32 +0700

    Add project files.

---
+++ AplikasiLatihanTiga3/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace AplikasiLatihanTiga3
+{
+    partial class frmLatihanTiga3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblNama = new System.Windows.Forms.Label();
+            this.lblPendidikan = new System.Windows.Forms.Label();
+            this.lblPekerjaan = new System.Windows.Forms.Label();
+            this.txtNama = new System.Windows.Forms.TextBox();
+            this.txtPesan1 = new System.Windows.Forms.TextBox();
+            this.txtPesan2 = new System.Windows.Forms.TextBox();
+            this.txtPesan3 = new System.Windows.Forms.TextBox();
+            this.cmbPendidikan = new System.Windows.Forms.ComboBox();
+            this.lstPekerjaan = new System.Windows.Forms.ListBox();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNama
+            // 
+            this.lblNama.AutoSize = true;
+            this.lblNama.Location = new System.Drawing.Point(39, 39);
+            this.lblNama.Name = "lblNama";
+            this.lblNama.Size = new System.Drawing.Size(51, 20);
+            this.lblNama.TabIndex = 0;
+            this.lblNama.Text = "Nama";
+            // 
+            // lblPendidikan
+            // 
+            this.lblPendidikan.AutoSize = true;
+            this.lblPendidikan.Location = new System.Drawing.Point(39, 90);
+            this.lblPendidikan.Name = "lblPendidikan";
+            this.lblPendidikan.Size = new System.Drawing.Size(87, 20);
+            this.lblPendidikan.TabIndex = 1;
+            this.lblPendidikan.Text = "Pendidikan";
+            // 
+            // lblPekerjaan
+            // 
+            this.lblPekerjaan.AutoSize = true;
+            this.lblPekerjaan.Location = new System.Drawing.Point(39, 144);
+            this.lblPekerjaan.Name = "lblPekerjaan";
+            this.lblPekerjaan.Size = new System.Drawing.Size(80, 20);
+            this.lblPekerjaan.TabIndex = 2;
+            this.lblPekerjaan.Text = "Pekerjaan";
+            // 
+            // txtNama
+            // 
+            this.txtNama.Location = new System.Drawing.Point(195, 39);
+            this.txtNama.Name = "txtNama";
+            this.txtNama.Size = new System.Drawing.Size(229, 26);
+            this.txtNama.TabIndex = 3;
+            // 
+            // txtPesan1
+            // 
+            this.txtPesan1.Enabled = false;
+            this.txtPesan1.Location = new System.Drawing.Point(43, 321);
+            this.txtPesan1.Name = "txtPesan1";
+            this.txtPesan1.Size = new System.Drawing.Size(381, 26);
+            this.txtPesan1.TabIndex = 4;
+            // 
+            // txtPesan2
+            // 
+            this.txtPesan2.Enabled = false;
+            this.txtPesan2.Location = new System.Drawing.Point(43, 353);
+            this.txtPesan2.Name = "txtPesan2";
+            this.txtPesan2.Size = new System.Drawing.Size(381, 26);
+            this.txtPesan2.TabIndex = 5;
+            // 
+            // txtPesan3
+            // 
+            this.txtPesan3.Enabled = false;
+            this.txtPesan3.Location = new System.Drawing.Point(43, 385);
+            this.txtPesan3.Name = "txtPesan3";
+            this.txtPesan3.Size = new System.Drawing.Size(381, 26);
+            this.txtPesan3.TabIndex = 6;
+            // 
+            // cmbPendidikan
+            // 
+            this.cmbPendidikan.FormattingEnabled = true;
+            this.cmbPendidikan.Items.AddRange(new object[] {
+            "SD",
+            "SMP",
+            "SMA",
+            "Diploma",
+            "Sarjana",
+            "Pascasarjana"});
+            this.cmbPendidikan.Location = new System.Drawing.Point(195, 87);
+            this.cmbPendidikan.Name = "cmbPendidikan";
+            this.cmbPendidikan.Size = new System.Drawing.Size(229, 28);
+            this.cmbPendidikan.TabIndex = 7;
+            // 
+            // lstPekerjaan
+            // 
+            this.lstPekerjaan.FormattingEnabled = true;
+            this.lstPekerjaan.ItemHeight = 20;
+            this.lstPekerjaan.Items.AddRange(new object[] {
+            "Pedagang",
+            "PNS",
+            "Dosen",
+            "Swasta",
+            "ABRI"});
+            this.lstPekerjaan.Location = new System.Drawing.Point(195, 144);
+            this.lstPekerjaan.Name = "lstPekerjaan";
+            this.lstPekerjaan.Size = new System.Drawing.Size(229, 144);
+            this.lstPekerjaan.TabIndex = 8;
+            this.lstPekerjaan.SelectedIndexChanged += new System.EventHandler(this.lstPekerjaan_SelectedIndexChanged);
+            // 
+            // btnOk
+            // 
+            this.btnOk.Location = new System.Drawing.Point(103, 417);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(250, 30);
+            this.btnOk.TabIndex = 9;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            this.btnOk.Click += new System.EventHandler(this.btnOk_Click);
+            // 
+            // frmLatihanTiga3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 479);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.lstPekerjaan);
+            this.Controls.Add(this.cmbPendidikan);
+            this.Controls.Add(this.txtPesan3);
+            this.Controls.Add(this.txtPesan2);
+            this.Controls.Add(this.txtPesan1);
+            this.Controls.Add(this.txtNama);
+            this.Controls.Add(this.lblPekerjaan);
+            this.Controls.Add(this.lblPendidikan);
+            this.Controls.Add(this.lblNama);
+            this.Name = "frmLatihanTiga3";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Latihan 3.3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNama;
+        private System.Windows.Forms.Label lblPendidikan;
+        private System.Windows.Forms.Label lblPekerjaan;
+        private System.Windows.Forms.TextBox txtNama;
+        private System.Windows.Forms.TextBox txtPesan1;
+        private System.Windows.Forms.TextBox txtPesan2;
+        private System.Windows.Forms.TextBox txtPesan3;
+        private System.Windows.Forms.ComboBox cmbPendidikan;
+        private System.Windows.Forms.ListBox lstPekerjaan;
+        private System.Windows.Forms.Button btnOk;
+    }
+}
+
